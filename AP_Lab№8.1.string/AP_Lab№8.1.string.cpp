274d3a44868cd219f75e3aee1a3694652c6fa6ea
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-30 02:43:26 +0200

    first commit

---
+++ AP_Lab№8.1.string/AP_Lab№8.1.string.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int Count(const string s) {
+    int k = 0;
+    size_t pos = 0;
+    while ((pos = s.find("while", pos)) != string::npos) {
+        k++;
+        pos++;
+    }
+    return k;
+}
+
+string Change(string& s) {
+    size_t pos = 0;
+    while ((pos = s.find("while", pos)) != string::npos)
+        s.replace(pos, 5, "***");
+    return s;
+}
+
+int main() {
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+    cout << "String contained " << Count(str) << " groups of 'while'" << endl;
+    string dest = Change(str);
+    cout << "Modified string (param) : " << str << endl;
+    cout << "Modified string (result): " << dest << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№8.1.string/AP_Lab№8.1.string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1 {
+    TEST_CLASS(StringTests) {
+public:
+    TEST_METHOD(TestStringCount) {
+        string str = "while doing while loop while";
+        int result = Count(str);
+        Assert::AreEqual(3, result);
+    }
+
+    TEST_METHOD(TestStringChange) {
+        string str = "while doing while loop while";
+        string result = Change(str);
+        Assert::AreEqual(string("*** doing *** loop ***"), result);
+    }
+    };
+}
